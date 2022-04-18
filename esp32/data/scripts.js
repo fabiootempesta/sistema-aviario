910b81c12381514d5cd8e42b4b4ac7a9bdd97a0f
Fábio Oliveira Tempesta <fabio.oliveira.tempesta@gmail.com>
2022-04-18 08:03:51 -0300

    front end

--- esp32/data/scripts.js
+++ esp32/data/scripts.js
@@ -15,18 +15,21 @@ var cv_actuator_opmode_exchanger_deltat;
 var cv_actuator_opmode_fan_temp_on;
 var cv_actuator_opmode_fan_temp_off;
 
-var cv_card_nebulizer = 1;
-var cv_card_exchanger = 1;
-var cv_card_fan = 1;
+function expandDiv(div_id) {
+  var screenMobile = document.getElementById("screen_mobile");
+  screenMobile.style.setProperty('display', 'none', 'important');
+  var div = document.getElementById(div_id);
+  if (div.style.display == "none !important") {
+    div.style.setProperty('display', 'block', 'important');
+  } else {
+    div.style.setProperty('display', 'none', 'important');
+  }
+}
 
-
-
-function buttonSetOpModeActuator(checkboxId, actuator_name) {
-  var checkbox = document.getElementById(checkboxId);
+function showOpMode(value, actuator_name) {
   var automatic_div = document.getElementById("automatic_" + actuator_name);
   var manual_div = document.getElementById("manual_" + actuator_name);
-
-  if(checkbox.checked){
+  if(value){
     automatic_div.style.setProperty('display', 'none', 'important');
     manual_div.style.setProperty('display', 'flex', 'important');
   }else{
@@ -35,7 +38,7 @@ function buttonSetOpModeActuator(checkboxId, actuator_name) {
   }
 }
 
-function ShowHideDivParam(radioCheck, actuator_name) {
+function showHideDivParam(radioCheck, actuator_name) {
   var divParam = document.getElementById("card_param_" + actuator_name);
   if (radioCheck) {
     divParam.classList.remove('hover');
@@ -48,25 +51,21 @@ function ShowHideDivParam(radioCheck, actuator_name) {
   }
 }
 
-
-
 function updateActuatorsState() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var array = this.responseText.split(" ");
       //array index = |0: trocador|  |1: ventilador|   |2: nebulizador|
-      cv_actuator_status_exchanger = array[0];
-      cv_actuator_status_fan = array[1];
-      cv_actuator_status_nebulizer = array[2];
-      document.getElementById("status_exchanger").innerHTML = cv_actuator_status_exchanger;
-      document.getElementById("status_fan").innerHTML = cv_actuator_status_fan;
-      document.getElementById("status_nebulizer").innerHTML = cv_actuator_status_nebulizer;
+      document.getElementById("status_exchanger").innerHTML = array[0];
+      document.getElementById("status_fan").innerHTML = array[1];
+      document.getElementById("status_nebulizer").innerHTML = array[2];
     }
   };
   xhttp.open("GET", "/actuators/status/getall", true);
   xhttp.send();
 }
+
 function updateActuatorsOpMode() { //atualizar o modo de operação dos atuadores
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -75,14 +74,19 @@ function updateActuatorsOpMode() { //atualizar o modo de operação dos atuadore
 
       //array index = |0: nebulizador|  |1: trocador de água|   |2: ventilador|
       document.getElementById("opmode_nebulizer").innerHTML = array[0];
+      showOpMode(array[0], "nebulizer");
+
       document.getElementById("opmode_exchanger").innerHTML = array[1];
+      showOpMode(array[1], "exchanger");
+
       document.getElementById("opmode_fan").innerHTML = array[2];
-      
+      showOpMode(array[2], "fan");
     }
   };
   xhttp.open("GET", "/actuators/opmode/getall", true);
   xhttp.send();
 }
+
 function updateSensors() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -91,17 +95,23 @@ function updateSensors() {
 
       //array index = |0: temperatura ambiente| |1: umidade ambiente relativa|  |2: temperatura da caixa| |3: temperatura do bebedouro|
 
-      updateSensor(array[3], "nipple_temperature", "°C", "-127.00");
-      updateSensor(array[2], "box_temperature", "°C", "-127.00");
-      updateSensor(array[1], "climate_humidity", "%", "nan");
-      updateSensor(array[0], "climate_temperature", "°C", "nan");
+      updateSensor(array[3], "mobile_nipple_temperature", "°C", "-127.00");
+      updateSensor(array[3], "computer_nipple_temperature", "°C", "-127.00");
 
+      updateSensor(array[2], "mobile_box_temperature", "°C", "-127.00");
+      updateSensor(array[2], "computer_box_temperature", "°C", "-127.00");
 
+      updateSensor(array[1], "mobile_climate_humidity", "%", "nan");
+      updateSensor(array[1], "computer_climate_humidity", "%", "nan");
+
+      updateSensor(array[0], "mobile_climate_temperature", "°C", "nan");
+      updateSensor(array[0], "computer_climate_temperature", "°C", "nan");
     }
   };
   xhttp.open("GET", "/sensors/getall", true);
   xhttp.send();
 }
+
 function updateSensor(value, sensor_span, unit_measurement, text_error) {
   if (value == text_error) {
     document.getElementById(sensor_span).innerHTML = "Falha na leitura do sensor!";
@@ -115,7 +125,7 @@ function updateSensor(value, sensor_span, unit_measurement, text_error) {
 //taxa de atualização de 5 segundos
 setInterval(function () {
   //atualizando o valor dos sensores
-  updateSensors();
+  /*updateSensors();
   updateActuatorsState();
-  updateActuatorsOpMode();
+  updateActuatorsOpMode();*/
 }, 5000);
